c120dad59e9aec0a97d8bf3394f9f2fd89064f1e
Daniela Poblete <daniela.pobletelagos@gmail.com>
2022-07-14 20:05:36 -0400

    Se agrega manejo con objetos y método array some()

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
-const userList = ['Tarea Ejemplo']
+const userList = [
+    {id: 1, name: 'Tarea Ejemplo', listId: 1}
+]
 
-function validateCard(card) {
-    if (!card) return false;
+function validateCard(cardName, list) {
+    if (!cardName) return false;
 
-    if (card.trim().length === 0) {
+    if (cardName.trim().length === 0) {
         alert('El nombre no puede ser vacío, intenta nuevamente');
         return false;
     }
 
-    for (let i = 0; i < userList.length; i++) {
-        if (userList[i].toLowerCase() === card.toLowerCase()) {
-            alert('Ya existe una tarjeta con ese nombre');
-            return false;
-        }
+    if (userList.some(el => (el.name.toLowerCase() === cardName.toLowerCase() && el.listId === list.id))) {
+        alert(`Ya existe una tarjeta con ese nombre en la lista "${list.name}"`);
+        return false;
     }
 
     return true;
 }
 
-function addCard(listId) {
+function addCard(list) {
     const cardName = prompt('Ingresa el nombre de la tarjeta');
-    const isValid = validateCard(cardName);
+    const isValid = validateCard(cardName, list);
 
     if (isValid) {
-        const listElement = document.getElementById(listId);
+        const listElement = document.getElementById('list'+ list.id);
         const card = document.createElement('li');
         card.appendChild(document.createTextNode(cardName));
         listElement.appendChild(card);
-        userList.push(cardName);
+        userList.push({id: userList.length +1, name: cardName, listId: list.id});
+
+        console.log(userList);
     }
 }
