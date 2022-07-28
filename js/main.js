27830cbaf46fbb58e6e6e1b172d43e491394b300
Daniela Poblete <daniela.pobletelagos@gmail.com>
2022-07-27 22:07:37 -0400

    Se elimina validación de nombre, se modifica manejo de inputs de usuario

--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ const userLists = [
         id: 1,
         name: 'Lista de tareas',
         cards: [
-            {id: 1, name: 'Tarea 1', listId: 1},
-            {id: 2, name: 'Tarea 2', listId: 1},
+            // {id: 1, name: 'Tarea 1', listId: 1},
+            // {id: 2, name: 'Tarea 2', listId: 1},
         ]
     },
     {
         id: 2,
         name: 'En proceso',
         cards: [
-            {id: 3, name: 'Tarea 3', listId: 2},
+            // {id: 3, name: 'Tarea 3', listId: 2},
         ]
     },
     {
@@ -35,10 +35,13 @@ function showLists() {
 
         list.cards.forEach(card => {
             cardsTemplate += `
-                    <li id="card${card.id}">
-                        <span data-id="${card.id}">${card.name}</span>
-                        <i class="edit-btn fa-solid fa-pencil" data-id="${card.id}"></i>
-                        <i class="delete-btn fa-solid fa-xmark" data-id="${card.id}"></i>
+                    <li id="card${card.id}" data-id="${card.id}">
+                        <span contenteditable="false" data-id="${card.id}">${card.name}</span>
+                        <div>
+                            <i class="edit-btn fa-solid fa-pencil" data-id="${card.id}"></i>
+                            <i class="move-btn fa-solid fa-arrows-up-down-left-right" data-id=${card.id}"></i>
+                            <i class="delete-btn fa-solid fa-xmark" data-id="${card.id}"></i>
+                        </div>                  
                     </li>
                 `;
         })
@@ -47,7 +50,7 @@ function showLists() {
             <div class="list">
                 <p>${list.name}</p>
                     <ul id="list${list.id}">
-                    ${cardsTemplate}
+                        ${cardsTemplate}
                     </ul>
                 <button class="add-btn" data-id="${list.id}">Agregar una tarjeta</button>
             </div>
@@ -60,10 +63,10 @@ function showLists() {
 function setEvents() {
     listsElement.addEventListener('click', (e) => {
         if (e.target) {
-            if (e.target.tagName === 'SPAN') {
+            if (e.target.classList.contains('move-btn')) {
                 moveCard(parseInt(e.target.getAttribute('data-id')));
             }
-            if (e.target.classList.contains('edit-btn')) {
+            if (e.target.classList.contains('edit-btn') || (e.target.tagName === 'LI')) {
                 editCard(parseInt(e.target.getAttribute('data-id')));
             }
             if (e.target.classList.contains('delete-btn')) {
@@ -76,55 +79,72 @@ function setEvents() {
     })
 }
 
-function validateCard(cardName, list) {
-    if (!cardName) return false;
-
-    if (cardName.trim().length === 0) {
-        alert('El nombre no puede ser vacío, intenta nuevamente');
-        return false;
-    }
-
-    return validateSameName(cardName, list);
-}
-
-function validateSameName(cardName, list) {
-    if (list.cards.some(obj => (obj.name.toLowerCase() === cardName.toLowerCase() && obj.listId === list.id))) {
-        alert(`Ya existe una tarjeta con el mismo nombre en la lista "${list.name}"`);
-        return false;
-    }
-
-    return true;
-}
-
+// TODO agregar eventos con tecla enter y escape
 function addCard(listId) {
     const list = userLists.find(obj => obj.id === listId);
-    const cardName = prompt('Ingresa el nombre de la tarjeta');
-    const isValid = validateCard(cardName, list);
+    const listElement = document.querySelector('#list' + list.id);
 
-    if (isValid) {
-        idCount += 1;
-        const listElement = document.querySelector('#list' + list.id);
+    listElement.innerHTML += `<li id="input" contenteditable="true"></li>`;
 
-        const html = `
-            <li id="card${idCount}">
-                <span data-id=${idCount}">${cardName}</span>
-                <i class="edit-btn fa-solid fa-pencil" data-id=${idCount}"></i>
-                <i class="delete-btn fa-solid fa-xmark" data-id=${idCount}"></i>
-            </li>
-        `;
+    const inputElement = document.querySelector('#input');
+    inputElement.focus();
 
-        const template = document.createElement('template');
-        template.innerHTML = html.trim();
-        const cardElement = template.content.firstElementChild;
-        listElement.appendChild(cardElement);
+    inputElement.addEventListener('focusout', e => {
+        if (inputElement.textContent.trim().length > 0) {
+            const cardName = inputElement.textContent;
+            idCount += 1;
 
-        list.cards.push({id: idCount, name: cardName, listId: list.id});
+            const html = `
+                <li id="card${idCount}" data-id="${idCount}">
+                    <span contenteditable="false" data-id=${idCount}">${cardName}</span>
+                    <div>
+                        <i class="edit-btn fa-solid fa-pencil" data-id=${idCount}"></i>
+                        <i class="move-btn fa-solid fa-arrows-up-down-left-right" data-id=${idCount}"></i>
+                        <i class="delete-btn fa-solid fa-xmark" data-id=${idCount}"></i>
+                    </div>
+                </li>
+            `;
 
-        console.log(`Tarjeta "${cardName}" agregada exitosamente`)
-        console.log('Nuevo array > ', userLists)
-    }
+            const template = document.createElement('template');
+            template.innerHTML = html.trim();
+            const cardElement = template.content.firstElementChild;
+            listElement.appendChild(cardElement);
+
+            list.cards.push({id: idCount, name: cardName, listId: list.id});
+
+            console.log(`Tarjeta "${cardName}" agregada exitosamente`)
+            console.log('Nuevo array > ', userLists)
+        }
+
+        inputElement.remove();
+    })
 }
 
+// TODO agregar eventos con tecla enter y escape
+function editCard(cardId) {
+    const userCards = userLists.map(obj => obj.cards).flat(); // revisar
+    const card = userCards.find(obj => obj.id === cardId);
+    const cardElement = document.querySelector('#card' + cardId);
+    const spanElement = cardElement.querySelector('span');
+
+    spanElement.setAttribute('contenteditable', 'true');
+    spanElement.focus();
+
+    spanElement.addEventListener('focusout', e => {
+        if (spanElement.textContent.trim().length > 0 && spanElement.textContent !== card.name) {
+            card.name = spanElement.textContent;
+
+            console.log(`Tarjeta "${card.name}" actualizada exitosamente`)
+            console.log('Nuevo array > ', userLists)
+        } else {
+            spanElement.textContent = card.name;
+        }
+
+        spanElement.setAttribute('contenteditable', 'false');
+    })
+}
+
+// TODO agregar manejo drag&drop
 function moveCard(cardId) {
     const newListId = parseInt(prompt('¿A qué lista deseas mover la tarea? Ingresa un número 1: "Lista de tareas", 2: "En proceso", 3: "Hecho"'));
     const userCards = userLists.map(obj => obj.cards).flat(); // revisar
@@ -133,19 +153,17 @@ function moveCard(cardId) {
     const newList = userLists.find(obj => obj.id === newListId);
 
     if (card.listId !== newListId && newListId > 0 && newListId <= 3) {
-        if (validateSameName(card.name, newList)) {
-            const cardElement = document.querySelector('#card' + card.id);
-            cardElement.remove();
-            const newListElement = document.querySelector('#list' + newListId);
-            newListElement.appendChild(cardElement);
+        const cardElement = document.querySelector('#card' + card.id);
+        cardElement.remove();
+        const newListElement = document.querySelector('#list' + newListId);
+        newListElement.appendChild(cardElement);
 
-            card.listId = newListId;
-            oldList.cards = oldList.cards.filter(obj => obj.id !== card.id);
-            newList.cards.push(card);
+        card.listId = newListId;
+        oldList.cards = oldList.cards.filter(obj => obj.id !== card.id);
+        newList.cards.push(card);
 
-            console.log(`Tarjeta "${card.name}" movida exitosamente`)
-            console.log('Nuevo array > ', userLists)
-        }
+        console.log(`Tarjeta "${card.name}" movida exitosamente`)
+        console.log('Nuevo array > ', userLists)
 
     } else if (card.listId === newListId) {
         alert('La tarea ya pertenece a esa lista');
@@ -159,29 +177,13 @@ function deleteCard(cardId) {
     const card = userCards.find(obj => obj.id === cardId);
     const list = userLists.find(obj => obj.id === card.listId);
     const cardElement = document.querySelector('#card' + cardId);
-    const confirmation = parseInt(prompt(`¿Estás seguro de querer borrar la tarjeta "${card.name}"? Ingresa 1 para confirmar`));
+    const confirmation = window.confirm(`¿Estás seguro que deseas borrar la tarjeta "${card.name}"?`);
 
-    if (confirmation === 1) {
+    if (confirmation) {
         cardElement.remove();
         list.cards = list.cards.filter(obj => obj.id !== card.id);
 
         console.log(`Tarjeta "${card.name}" eliminada exitosamente`)
-        console.log('Nuevo array > ', userLists)
-    }
-}
-
-function editCard(cardId) {
-    const userCards = userLists.map(obj => obj.cards).flat(); // revisar
-    const card = userCards.find(obj => obj.id === cardId);
-    const list = userLists.find(obj => obj.id === card.listId);
-    const newName = prompt('Ingresa la nueva información de la tarjeta');
-
-    if (validateCard(newName, list)) {
-        const cardElement = document.querySelector('#card' + cardId);
-        cardElement.querySelector('span').textContent = newName;
-        card.name = newName;
-
-        console.log(`Tarjeta "${card.name}" actualizada exitosamente`)
         console.log('Nuevo array > ', userLists)
     }
 }
